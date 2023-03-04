a846a0b2782bf4a801ef4f11d0b7c49378148d7d
Mauzerov <mauzer.official.1@gmail.com>
2023-03-04 19:45:32 +0100

    extract interfaces to separate files

---
+++ src/Direction.ts
@@ -0,0 +1,19 @@
+enum Direction {
+    NORTH = 0,
+    SOUTH,
+    EAST,
+    WEST,
+}
+
+namespace Direction {
+    function opposite(direction: Direction): Direction {
+        switch (direction) {
+            case Direction.NORTH: return Direction.SOUTH;
+            case Direction.SOUTH: return Direction.NORTH;
+            case Direction.EAST: return Direction.WEST;
+            case Direction.WEST: return Direction.EAST;
+        }
+    }
+}
+
+export default Direction;
---
+++ src/IGame.ts
@@ -0,0 +1,15 @@
+export interface Position {
+    x: number,
+    y: number
+}
+
+export interface GameProps {
+}
+
+export interface GameState {
+    player: Position,
+    boxes: Position[],
+    map: string,
+
+    targets: Position[]
+}
