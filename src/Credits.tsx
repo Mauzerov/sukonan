3c8dbb5daa7df1534c2c7a6ac8a26f2f80aae31f
Mauzerov <mauzer.official.1@gmail.com>
2023-03-05 13:45:28 +0100

    add credits element

---
+++ src/Credits.tsx
@@ -0,0 +1,15 @@
+export function Credits(props? : {}) {
+    return (
+        <div className="credits">
+            <h1>Credits</h1>
+            <p>Created by <a href="https://github.com/Mauzerov" target="_blank">Mateusz Mazurek</a></p>
+            <p>man pulling cart by Vectors Point from <a href="https://thenounproject.com/browse/icons/term/man-pulling-cart/" target="_blank" title="man pulling cart Icons">Noun Project</a></p>
+            <p>Game Icons <a href="https://www.svgrepo.com" target="_blank">SvgRepo</a><br />
+                <ul>
+                    <li>Help Icon: @HashiCorp</li>
+                    <li>Restart Icon: @Ionicons</li>
+                </ul>
+            </p>
+        </div>
+    );
+}
