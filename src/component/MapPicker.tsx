b7036b3238709487255a52bb382e682e6ea63aab
Mauzerov <mauzer.official.1@gmail.com>
2023-03-21 20:05:10 +0100

    display information about lack of own maps

--- src/component/MapPicker.tsx
+++ src/component/MapPicker.tsx
@@ -2,46 +2,58 @@ import {MapGrid} from "./MapGrid";
 import {filterBoxesAndTargets, filterPorters} from "../util/GameUtil";
 import React from "react";
 import "../styles/Game.scss"
+import "../styles/MapPicker.scss"
+import {Link} from "react-router-dom";
 
 export function MapPicker(
     props: {
         onMapSelect?: (map: number) => void
     }
 ) {
+    const maps = JSON.parse(localStorage.getItem('sukonan-maps') || '[]')
     return (
-        <div style={{
-            display: 'grid',
-            width: "100%",
-            gridTemplateColumns: `repeat(5, auto)`,
-            gap: "1rem",
-            // aspectRatio: "1 / 1",
-        } as React.CSSProperties}>
-            {JSON.parse(localStorage.getItem('sukonan-maps') || '[]').map((map: string[], i: number) => {
-                const gridSize = {
-                    x: map[0].length,
-                    y: map.length,
-                }
-                const mapString = map.join('');
-                return (
-                    <div style={{
-                        "--cell-size": `auto`,
-                    } as React.CSSProperties}
-                    onClick={() => {props.onMapSelect?.(i)}}
-                    >
-                        <MapGrid
-                            key={i}
-                            map={mapString}
-                            gameElements={{
-                                player: {
-                                    x: mapString.indexOf('S') % gridSize.x,
-                                    y: Math.floor(mapString.indexOf('S') / gridSize.x),
-                                },
-                                porters: filterPorters(mapString, gridSize),
-                                ...filterBoxesAndTargets(mapString, gridSize)
-                            }}
-                            gridSize={gridSize}/>
-                    </div>)
-            })}
-        </div>
+        <>
+            {!maps.length && <div
+                className="map-picker__no-maps">
+                No maps found. Create one in the editor.
+                <Link to="/editor" className="button">Editor</Link>
+            </div>}
+            {maps && <div
+                className={"map-picker"}
+                style={{
+                display: 'grid',
+                width: "100%",
+                gap: "1rem",
+                "--own-map-count": maps.length,
+                // aspectRatio: "1 / 1",
+            } as React.CSSProperties}>
+                {maps && maps.map((map: string[], i: number) => {
+                    const gridSize = {
+                        x: map[0].length,
+                        y: map.length,
+                    }
+                    const mapString = map.join('');
+                    return (
+                        <div style={{
+                            "--cell-size": `auto`,
+                        } as React.CSSProperties}
+                        onClick={() => {props.onMapSelect?.(i)}}
+                        >
+                            <MapGrid
+                                key={i}
+                                map={mapString}
+                                gameElements={{
+                                    player: {
+                                        x: mapString.indexOf('S') % gridSize.x,
+                                        y: Math.floor(mapString.indexOf('S') / gridSize.x),
+                                    },
+                                    porters: filterPorters(mapString, gridSize),
+                                    ...filterBoxesAndTargets(mapString, gridSize)
+                                }}
+                                gridSize={gridSize}/>
+                        </div>)
+                })}
+            </div>}
+        </>
     )
 }
