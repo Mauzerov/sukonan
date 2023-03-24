91f3fd535b55c57f11f0f56a11fb7ba3ced7480d
Mauzerov <mauzer.official.1@gmail.com>
2023-03-24 10:26:01 +0100

    style credits page

--- src/component/Credits.tsx
+++ src/component/Credits.tsx
@@ -1,21 +1,19 @@
 import React from "react";
+import "../styles/Credits.scss"
 
 export default function Credits() {
     return (
-        <div>
-            <h1>Credits</h1>
-            <p>Created by <a href="https://github.com/Mauzerov" target="_blank" rel="noreferrer">Mateusz Mazurek</a></p>
-            <p>man pulling cart by Vectors Point from
-                <a href="https://thenounproject.com/browse/icons/term/man-pulling-cart/" target="_blank" title="man pulling cart Icons" rel="noreferrer">Noun Project</a>
-            </p>
-            <p>Icons <a href="https://www.svgrepo.com" target="_blank" rel="noreferrer">SvgRepo</a><br />
+        <div className="credits">
+            <h1 className="title">Credits</h1>
+            <div className="author">Created by <a href="https://github.com/Mauzerov" target="_blank" rel="noreferrer">Mateusz Mazurek</a></div>
+            <div className="icons svg-repo">Icons from <a href="https://www.svgrepo.com" target="_blank" rel="noreferrer">SvgRepo</a>:
                 <ul>
                     <li>Help Icon: @HashiCorp</li>
                     <li>Restart Icon: @Ionicons</li>
                     <li>Teleporter Spiral Icon: @game-icons.net</li>
                     <li>Save Icon: @Ananthanath A X Kalaiism</li>
                 </ul>
-            </p>
+            </div>
         </div>
     )
 }
