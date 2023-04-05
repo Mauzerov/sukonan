6bf53f614ac10688c240d69386a1cfd39e41ffff
Mauzerov <mauzer.official.1@gmail.com>
2023-04-05 08:10:12 +0200

    add delay to win message buttons

--- src/component/WinAlert.tsx
+++ src/component/WinAlert.tsx
@@ -1,17 +1,28 @@
 import '../styles/WinAlert.scss'
 import {WinAlertProps} from "../ts/IGame";
+import {useState, useEffect} from "react";
 
 
 export default function WinAlert(
     props: WinAlertProps & {map: number}
 ) {
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        if (disabled) {
+            setTimeout(() => {
+                setDisabled(false);
+            }, 250);
+        }
+    }, [disabled])
+
     return (
         <div className="win-alert">
             <div className="win-alert__title">{props.title}</div>
             {props.text && <div className="win-alert__text">{props.text}</div>}
             <div className="win-alert__buttons">
                 {props.buttons.map((button, i) => (
-                    <button key={i} onClick={() => {
+                    <button disabled={disabled} key={i} onClick={() => {
                         if (button.onClick)
                             button.onClick(props.map);
                     }}>{button.children}</button>
